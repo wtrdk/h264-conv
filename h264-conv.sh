d3a3f4e8bc6043ce70c7946cbb8907f54461723e
Wouter Dijk <wouter.dijk@gmail.com>
2019-04-05 08:59:26 +0200

    more bugfixes

--- h264-conv.sh
+++ h264-conv.sh
@@ -3,11 +3,11 @@
 DIRECTORY="h264"
 
 if [ ! -d "$DIRECTORY" ]; then
-    mkdir $DIRECTORY # Control will enter here if $DIRECTORY doesn't exist.
+    mkdir "$DIRECTORY" # Control will enter here if $DIRECTORY doesn't exist.
 fi
 
 for i in *.mkv; do
     
-    ffmpeg -i "$i" -map 0 -c copy -c:v libx264 -crf 23 -preset medium $DIRECTORY/"$i"
+    ffmpeg -i "$i" -map 0 -c copy -c:v libx264 -crf 23 -preset medium "$DIRECTORY"/"$i"
     
 done
